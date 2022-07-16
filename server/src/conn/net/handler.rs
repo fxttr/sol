e7aee4a4405b67fa8af40377aa5ef77ab867e66e
Florian <fb@fx-ttr.de>
2022-07-16 09:25:25 +0200

    Adding clap, init handler structure

--- server/src/conn/net/handler.rs
+++ server/src/conn/net/handler.rs
@@ -1,5 +1,5 @@
-/**
- * Copyright (c) 2022, Florian Buestgens
+/*
+ * Copyright (c) 2022, Florian Büstgens
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -11,10 +11,10 @@
  *        this list of conditions and the following disclaimer in the
  *        documentation and/or other materials provided with the distribution.
  *
- * THIS SOFTWARE IS PROVIDED BY Florian Buestgens ''AS IS'' AND ANY
+ * THIS SOFTWARE IS PROVIDED BY Florian Büstgens ''AS IS'' AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL Florian Buestgens BE LIABLE FOR ANY
+ * DISCLAIMED. IN NO EVENT SHALL Florian Büstgens BE LIABLE FOR ANY
  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -23,30 +23,20 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-mod core;
-mod conn;
+use crate::conn::Listener;
 
-use clap::Parser;
-use conn::net::handler::NetHandler;
-use conn::Listener;
-
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-    #[clap(short, long, value_parser)]
-    handler: String,
-
-    #[clap(short, long, value_parser)]
-    config: String
+pub struct NetHandler {
+    
 }
 
-fn main() {
-    let args = Args::parse();
+impl NetHandler {
+    pub fn new() -> Self {
+	Self {  }
+    }
+}
 
-    let handler = match args.handler.as_str() {
-	"net" => NetHandler::new(),
-	_ => NetHandler::new()
-    };
-
-    handler.hear();
+impl Listener for NetHandler {
+    fn hear(&self) {
+	todo!()
+    }
 }
